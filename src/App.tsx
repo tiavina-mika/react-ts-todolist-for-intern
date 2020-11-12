ed67d7bdd02a7b39a5a3a7e57f33d822bbe1d888
TiavinaMichael <ralainirinatiavina@gmail.com>
2020-11-12 12:25:03 +0300

    add features

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { createUseStyles } from "react-jss";
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { createUseStyles, useTheme } from "react-jss";
+import { FaCheck, FaTimes, FaTrashAlt } from 'react-icons/fa';
 import "./styles.css";
 
 const useStyles = createUseStyles((theme: any) => ({
@@ -22,6 +24,9 @@ const useStyles = createUseStyles((theme: any) => ({
     composes: 'stretchSelf',
     borderRadius: 3,
     padding: theme.spacing(3),
+    '&:focus': {
+      outline: 'none',
+    }
   },
   button: {
     composes: 'stretchSelf',
@@ -29,6 +34,9 @@ const useStyles = createUseStyles((theme: any) => ({
     paddingTop: theme.spacing(3),
     paddingBottom: theme.spacing(3),
     marginTop: theme.spacing(2),
+    '&:focus': {
+      outline: 'none',
+    }
   },
   items: {
     composes: 'flexColumn center spaceBetween flex1 stretchSelf',
@@ -41,19 +49,55 @@ const useStyles = createUseStyles((theme: any) => ({
   },
   text: {
     fontFamily: 'Roboto, sans-serif'
+  },
+  actions: {
+    composes: 'flexRow center',
+  },
+  actionButton: {
+    composes: 'flexRow center',
+    backgroundColor: 'transparent',
+    border: 'none',
+    '&:focus': {
+      outline: 'none',
+    }
   }
 }))
+/*
+  ['tsxfd', 'exxt'],
+  [{ text: 'fsxfs', check: false}]
+*/
+
+interface Item {
+  text: string;
+  checked: boolean;
+}
 const App = () => {
+  const theme = useTheme();
   const classes = useStyles();
-  const [text, setText] = React.useState<string>('');
-  const [items, setItems] = React.useState<string[]>([]);
+  const [text, setText] = useState<string>('');
+  const [items, setItems] = useState<Item[]>([]);
 
-  const handleChange = () => {
-
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
   };
-  const handleSubmit = () => {
-
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text) setItems([...items, { text, checked: false }]);
+    setText('');
   };
+  
+  const handleDelete = (index: number): void => {
+    const newItems = items.filter((_, i) => i !== index);
+    setItems([...newItems]);
+  }
+
+  const handleCheck = (index: number): void => {
+    const newItems = [...items];
+    newItems.map((item, i) => {
+      if (i === index) item.checked = !item.checked;
+    })
+    setItems([...newItems]);
+  }
 
   return (
       <div className={classes.root}>
@@ -68,10 +112,35 @@ const App = () => {
               <button type="submit" className={classes.button}>Save</button>
           </form>
           <div className={classes.items}>
-            <div className={classes.item}>
-              <span className={classes.text}>Text Exemple</span>
-              <button>x</button>
-            </div>
+            { items.length > 0
+              ? [items.map((item, index) => (
+              <div className={classes.item} key={item.text}>
+                <span className={classes.text}>
+                  {item.text}
+                </span>
+              <div className={classes.actions}>
+                <button 
+                  className={classes.actionButton} 
+                  type="button"
+                  onClick={() => handleCheck(index)}
+                >
+                  {item.checked 
+                    ? <FaCheck size={22} color={(theme as any)?.colors.active} /> 
+                    : <FaTimes size={22} />
+                  }
+                </button>
+                <button 
+                  className={classes.actionButton} 
+                  type="button"
+                  onClick={() => handleDelete(index)}
+                >
+                  <FaTrashAlt size={18} />
+                </button>
+              </div>
+              </div>
+            ))]
+            : <span className={classes.text}>No Todo Yet </span>
+          }
           </div>
         </div>
       </div>
