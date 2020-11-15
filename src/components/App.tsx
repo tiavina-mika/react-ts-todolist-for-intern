c3d345e5d202df0c767f8a0eb6ff9d4846adf872
TiavinaMichael <ralainirinatiavina@gmail.com>
2020-11-15 12:25:55 +0300

    add link and profile

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,9 +9,31 @@ import "../styles.css";
 const useStyles = createUseStyles((theme: any) => ({
   root: {
     composes: 'flexColumn center justifyCenter',
-    height: '100vh'
+    height: '100vh',
+    fontFamily: 'Roboto, sans-serif',
   },
-  container: {},
+  content: {
+    composes: 'flexColumn center justifyCenter stretchSelf',
+    flex: 1,
+  },
+  body: {},
+  titleContainer: {
+    fontStyle: 'italic',
+    fontWeight: 'normal'
+  },
+  footer: {
+    composes: 'flexColumn center justifyCenter stretchSelf',
+    height: 80,
+    fontSize: 18,
+    letterSpacing: 1.05,
+  },
+  link: {
+    color: theme.colors.active,
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'underline',
+    }
+  },
 }))
 
 
@@ -21,9 +43,17 @@ const App = () => {
   return (
     <TodoProvider>
       <div className={classes.root}>
-        <div className={classes.container}>
-          <Form />
-          <Todos />
+        <div className={classes.content}>
+          <div className={classes.titleContainer}>
+            <h1>My Todo list</h1>
+          </div>
+          <div className={classes.body}>
+            <Form />
+            <Todos />
+          </div>
+        </div>
+        <div className={classes.footer}>
+          <span>By <a href="https://www.linkedin.com/in/tiavina-michael-ralainirina/"  className={classes.link}>Tiavina Michael R.</a></span>
         </div>
       </div>
     </TodoProvider>
